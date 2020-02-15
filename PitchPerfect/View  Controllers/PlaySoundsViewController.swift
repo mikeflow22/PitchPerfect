261dca76d146161b236c595a494e3cb46a9a5c5e
Michael R.F <mrflowers22@uwalumni.com>
2020-02-15 15:20:29 -0800

    added buttons with images on playSoundViewController. connected the outlets and actions

--- PitchPerfect/View  Controllers/PlaySoundsViewController.swift
+++ PitchPerfect/View  Controllers/PlaySoundsViewController.swift
@@ -12,12 +12,30 @@ class PlaySoundsViewController: UIViewController {
 
     var recordedAudioURL: URL!
     
+    @IBOutlet weak var snailButton: UIButton!
+    @IBOutlet weak var chipmunkButton: UIButton!
+    @IBOutlet weak var rabbitButton: UIButton!
+    @IBOutlet weak var vaderButton: UIButton!
+    @IBOutlet weak var echoButton: UIButton!
+    @IBOutlet weak var reverbButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func playSoundForButton(_ sender: UIButton) {
+        print("Play Sound Button Pressed")
+    }
+
+    @IBAction func stopButtonPressed(_ sender: AnyObject) {
+        print("Stop Audio Button Pressed")
+    }
 
     /*
     // MARK: - Navigation
