78fb63b3e115c8fb18fba3231332c47d005ad995
Michael R.F <mrflowers22@uwalumni.com>
2020-02-15 16:38:27 -0800

    added AudioRecorder class

---
+++ PitchPerfect/Model/AudioRecorder.swift
@@ -0,0 +1,13 @@
+//
+//  AudioRecorder.swift
+//  PitchPerfect
+//
+//  Created by Michael Flowers on 2/15/20.
+//  Copyright © 2020 Michael Flowers. All rights reserved.
+//
+
+import AVFoundation
+
+struct AudioRecorder {
+    var avAudioRecorder: AVAudioRecorder
+}
